6826ac594764b234415ac78cdaa8793cb48cc543
tired_ <omaralim06@gmail.com>
2026-09-15 15:21:01 -0700

    feat: edge-case testing for div function

--- sample-functions.test.js
+++ sample-functions.test.js
@@ -6,3 +6,19 @@ test('Testing div -- success', () => {
   expect(target).toBe(result);
 });
 
+test('Testing div -- negative numbers success', () => {
+  const target = -6;
+  const result = myFunctions.div(-30, 5);
+  expect(target).toBe(result);
+});
+
+test('Testing div -- division by zero', () => {
+    const result = myFunctions.div(30, 0);
+    expect(result).toBe(Infinity);
+});
+
+test('Testing div -- division of zero by zero', () => {
+    const result = myFunctions.div(0, 0);
+    expect(result).toBe(NaN);
+});
+
